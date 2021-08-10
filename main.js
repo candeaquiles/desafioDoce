ab8b912781a1a8465a77411597895912fa23a382
candeaquiles <80926300+candeaquiles@users.noreply.github.com>
2021-08-10 20:46:42 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+$("#btn1").click(function () {
+    $(".divFather").append(`
+    <p>Usted ha elegido la promocion nº1 </p>`
+    
+    )
+    
+})
+
+$("#btn2").click(function(){
+    $(".divFather").append(
+        `
+        <p>Usted ha escodigo la promocion nº2</p>`
+    )
+})
